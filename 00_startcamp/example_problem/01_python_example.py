3afc455b90907bc83dd44f6187eed8b4cbcf3156
Gagle637 <gagle637@gmail.com>
2019-07-15 17:47:48 +0900

    jupyter_python 2019-07-15

--- 00_startcamp/example_problem/01_python_example.py
+++ 00_startcamp/example_problem/01_python_example.py
@@ -12,11 +12,20 @@
 # 	Output=a + a*11 + a*111 + a*1111
 # 	print(f' {number}, {Output}')
 
-# 4번쨰 시도
-number = input('')
-a=int(number)
-if 0<a and 10>a:
-	Output=a + a*11 + a*111 + a*1111
-	print(Output)
+# # 4번쨰 시도
+# number = input('')
+# a=int(number)
+# if 0<a and 10>a:
+# 	Output=a + a*11 + a*111 + a*1111
+# 	print(Output)
 
 # 성공 ps. 아웃풋만 나올것
+
+N= int(input())
+for i in range(1,N+1):
+	if i/3:
+		print(i, end=' ')
+	elif i%3=0 and i%11==0:
+		print('--', end=' ')
+	else:
+		print('-', end=' ')
