4c195e1317f5fb97187d6384dad6b82c05d835a0
Gagle637 <gagle637@gmail.com>
2019-09-19 18:03:10 +0900

    2019-09-19_1:N

--- 03_django/02_django_crud/articles/views.py
+++ 03_django/02_django_crud/articles/views.py
@@ -1,7 +1,7 @@
 from IPython import embed
 from django.core.exceptions import ValidationError
 from django.shortcuts import render, redirect
-from .models import Article
+from .models import Article, Comment
 
 
 # Create your views here.
@@ -25,24 +25,23 @@ def create(request):
     else:
         return render(request, 'articles/create.html')
 
-    
-
-def detail(request, pk):
-    article = Article.objects.get(pk=pk)
-    article.get_absolute_url()
-    context ={'article': article,}
+def detail(request, article_pk):
+    article = Article.objects.get(pk=article_pk)
+    # article.get_absolute_url()
+    comments = article.comment_set.all()
+    context ={'article': article, 'comments': comments}
     return render(request, 'articles/detail.html', context)
 
-def delete(request, pk):
-    article = Article.objects.get(pk=pk)
+def delete(request, article_pk):
+    article = Article.objects.get(pk=article_pk)
     if request.method == 'POST':
         article.delete()
         return redirect('articles:index')
     else:
         redirect(article)
 
-def update(request, pk):
-    article = Article.objects.get(pk=pk)
+def update(request, article_pk):
+    article = Article.objects.get(pk=article_pk)
     if request.method == 'POST':
         article.title = request.POST.get('title')
         article.content = request.POST.get('content')
@@ -51,3 +50,29 @@ def update(request, pk):
     else:
         context = {'article': article}
         return render(request, 'articles/update.html', context)
+
+def comments_create(request, article_pk):
+    # 댓글을 달 게시글
+    article = Article.objects.get(pk=article_pk)
+    if request.method == 'POST':
+        #form 에서 넘어온 댓글 정보
+        content = request.POST.get('content')
+        # 댓글 생성 및 저장
+        comment = Comment(article=article, content=content,)
+        comment.save()
+        return redirect(article)
+        # return redirect('articles:detail', article.pk)
+        # return redirect('articles:detail', article_pk)
+    else:
+        return redirect(article)
+
+def comment_delete(request, article_pk,comment_pk):
+    comment = Comment.objects.get(id=comment_pk)
+    # article = Article.objects.get(pk=article_pk)
+    if request.method == 'POST':
+        comment.delete()
+        return redirect('articles:detail', article_pk)
+        # return redirect(article)
+    else:
+        return redirect('articles:detail', article_pk)
+        # return redirect(article)
