8e6d895e50b67181d53490811799282e01765877
Gagle637 <gagle637@gmail.com>
2019-10-21 17:48:44 +0900

    2019-10-21 User

--- 03_django/03_django_form/articles/models.py
+++ 03_django/03_django_form/articles/models.py
@@ -23,7 +23,6 @@ class Comment(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    
     class Meta:
         ordering = ('-pk',)
     def __str__(self):
