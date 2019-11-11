c2ddf386299d3ec1f931ef819b7a305362521cf6
Gagle637 <gagle637@gmail.com>
2019-11-11 17:56:22 +0900

    yotube project

---
+++ 06_Vue/04_Vue_youtube_project/yotube-browser/src/components/VideoList.vue
@@ -0,0 +1,36 @@
+<template>
+  <ul class="col-lg-4 list-group">
+    <video-list-item v-for="video in videos" 
+    :key="video.etag" 
+    :video="video"
+    @videoSelect="onVideoSelect"
+    >
+    </video-list-item>
+  </ul>
+</template>
+
+<script>
+  import VideoListItem from './VideoListItem.vue'
+  export default {
+    name: 'VideoList',
+    components:{
+      VideoListItem,
+    },
+    methods: {
+      onVideoSelect(video) {
+        this.$emit('videoSelect', video)
+      }
+    },
+    props: {
+      videos: {
+        type: Array,
+        required: true,
+      }
+    }
+  }
+  
+</script>
+
+<style>
+
+</style>
