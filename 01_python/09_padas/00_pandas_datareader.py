a917bbc78f9db822ef2b2704c56b7a361d815606
Gagle637 <gagle637@gmail.com>
2020-08-20 22:14:29 +0900

    Add Pandas_datareader || 2020 08 20

---
+++ 01_python/09_padas/00_pandas_datareader.py
@@ -0,0 +1,19 @@
+from pandas_datareader import data as web
+import datetime
+import matplotlib.pyplot as plt
+
+start = datetime.datetime(2015, 12, 1)
+end = datetime.datetime(2016, 3, 1)
+
+# 애플: AAPL
+# 구글: GOOG
+# 삼성: 005930.KS
+ 
+gs = web.get_data_yahoo('005930.KS')
+gs.tail()
+
+plt.figure(figsize=(15, 5))
+plt.plot(gs['Close'])
+plt.show()
+
+# gs['Close'].plot(figsize=(15, 5))
